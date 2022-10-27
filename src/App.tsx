a9850a0167f4a5376e938256df52e2d73e8be738
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-27 13:13:18 +0200

    Created routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import {Home} from "./views/Home/Home";
+import {Contact} from "./views/Contact/Contact";
+import {Portfolio} from "./views/Portfolio/Portfolio";
+import {Cooperation} from "./views/Cooperation/Cooperation";
+import {Technologies} from "./views/ Technologies/Technologies";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element= {<Home />}/>
+                <Route path="/contact" element= {<Contact />}/>
+                <Route path="/about-me" element= {<Contact />}/>
+                <Route path="/portfolio" element= {<Portfolio />}/>
+                <Route path="/cooperation" element= {<Cooperation />}/>
+                <Route path="/technologies" element= {<Technologies />}/>
+            </Routes>
+        </div>
+    );
 }
-
-export default App;
