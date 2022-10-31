b08d6c9fd22f113a317c9d1d99bf10b46a4851eb
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-31 12:09:01 +0100

    Created active view context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import {Home} from "./views/Home/Home";
 import {Contact} from "./views/Contact/Contact";
 import {Portfolio} from "./views/Portfolio/Portfolio";
 import {Cooperation} from "./views/Cooperation/Cooperation";
-import {Technologies} from "./views/ Technologies/Technologies";
+import {Technologies} from "./views/Technologies/Technologies";
+import {AboutMe} from "./views/AboutMe/AboutMe";
+import {ActiveViewContext} from "./contexts/active-view-context";
 
 export const App = () => {
+    const [activeView, setActiveView] = useState('home');
+
     return (
+        <ActiveViewContext.Provider value={{ activeView, setActiveView }}>
         <div className="App">
             <Routes>
                 <Route path="/" element= {<Home />}/>
                 <Route path="/contact" element= {<Contact />}/>
-                <Route path="/about-me" element= {<Contact />}/>
+                <Route path="/about-me" element= {<AboutMe />}/>
                 <Route path="/portfolio" element= {<Portfolio />}/>
                 <Route path="/cooperation" element= {<Cooperation />}/>
                 <Route path="/technologies" element= {<Technologies />}/>
             </Routes>
         </div>
+        </ActiveViewContext.Provider>
     );
 }
