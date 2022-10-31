7d8cf8415e044d6d8fc7b319a174d27a394fdf1c
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-31 16:03:28 +0100

    Added routes to buttons.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ export const Footer = () => {
     return(
         <div className='footer-box'>
             <div className='footer-btn-box'>
-                <div className='footer-btn'>
+                <div className='footer-btn' onClick={() => window.location.href = "https://github.com/lukaszambrozewski"}>
                     <Icon icon="akar-icons:github-fill" />
                 </div>
-                <div className='footer-btn'>
+                <div className='footer-btn' onClick={() => window.location.href = "https://www.linkedin.com/in/%C5%82ukasz-ambro%C5%BCewski-943a01222/"}>
                     <Icon icon="akar-icons:linkedin-fill" />
                 </div>
             </div>
