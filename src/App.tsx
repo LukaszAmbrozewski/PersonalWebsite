752a4e92408951132ca2134126d00b7bdcf94d10
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-11-02 09:50:28 +0100

    Added language context to button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { Route, Routes } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
 import {Home} from "./views/Home/Home";
 import {Contact} from "./views/Contact/Contact";
@@ -8,22 +8,40 @@ import {Cooperation} from "./views/Cooperation/Cooperation";
 import {Technologies} from "./views/Technologies/Technologies";
 import {AboutMe} from "./views/AboutMe/AboutMe";
 import {ActiveViewContext} from "./contexts/active-view-context";
+import {LanguageContext} from "./contexts/language-context";
+import * as pl from './lang/pl.json'
+import * as de from './lang/de.json'
+import * as en from './lang/en.json'
 
 export const App = () => {
     const [activeView, setActiveView] = useState('home');
+    const [activeLanguage, setActiveLanguage] = useState('pl');
+    const [desc, setDesc] = useState(pl);
+
+    useEffect(() => {
+        if(activeLanguage === 'pl'){
+            setDesc(pl)
+        } else if (activeLanguage === 'en'){
+            setDesc(en)
+        } else if (activeLanguage === 'de'){
+            setDesc(de)
+        }
+    },[activeLanguage])
 
     return (
-        <ActiveViewContext.Provider value={{ activeView, setActiveView }}>
-        <div className="App">
-            <Routes>
-                <Route path="/" element= {<Home />}/>
-                <Route path="/contact" element= {<Contact />}/>
-                <Route path="/about-me" element= {<AboutMe />}/>
-                <Route path="/portfolio" element= {<Portfolio />}/>
-                <Route path="/cooperation" element= {<Cooperation />}/>
-                <Route path="/technologies" element= {<Technologies />}/>
-            </Routes>
-        </div>
+        <ActiveViewContext.Provider value={{activeView, setActiveView}}>
+            <LanguageContext.Provider value={{desc, activeLanguage, setActiveLanguage}}>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/contact" element={<Contact/>}/>
+                        <Route path="/about-me" element={<AboutMe/>}/>
+                        <Route path="/portfolio" element={<Portfolio/>}/>
+                        <Route path="/cooperation" element={<Cooperation/>}/>
+                        <Route path="/technologies" element={<Technologies/>}/>
+                    </Routes>
+                </div>
+            </LanguageContext.Provider>
         </ActiveViewContext.Provider>
     );
 }
--- src/components/MoreAboutMeBtn/MoreAboutMeBtn.tsx
+++ src/components/MoreAboutMeBtn/MoreAboutMeBtn.tsx
@@ -2,15 +2,17 @@ import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
 import './MoreAboutMeBtn.css'
 import {ActiveViewContext} from "../../contexts/active-view-context";
+import {LanguageContext} from "../../contexts/language-context";
 
 export const MoreAboutMeBtn = () => {
     const {setActiveView} = useContext(ActiveViewContext)
+    const {desc} = useContext(LanguageContext);
 
     const changeActiveView = (view: string) => {
         setActiveView(view)
     }
 
     return(
-        <Link to='/about-me' onClick={() => changeActiveView('about-me')} className='more-about-me-btn'>Więcej o mnie</Link>
+        <Link to='/about-me' onClick={() => changeActiveView('about-me')} className='more-about-me-btn'>{desc["main-about-me-home-box-btn"]}</Link>
     )
 }
