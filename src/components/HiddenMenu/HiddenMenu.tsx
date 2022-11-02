8ebf5e6c8d6a84084a4c62e86b62c4c6515a4702
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-11-02 09:40:16 +0100

    Renamed component.

--- src/components/HiddenMenu/HiddenMenu.tsx
+++ src/components/HiddenMenu/HiddenMenu.tsx
@@ -1,12 +1,15 @@
 import React, {useContext, useState} from 'react';
 import {Icon} from "@iconify/react";
-import './ShowMenuBtn.css';
+import './HiddenMenu.css';
 import {Link} from "react-router-dom";
 import {ActiveViewContext} from "../../contexts/active-view-context";
+import {LanguageContext} from "../../contexts/language-context";
+import {SelectLanguage} from "../SelectLanguage/SelectLanguage";
 
-export const ShowMenuBtn = () => {
+export const HiddenMenu = () => {
     const [showOptions, setShowOptions] = useState<boolean>(false);
     const {activeView, setActiveView} = useContext(ActiveViewContext)
+    const {desc} = useContext(LanguageContext);
 
     const show = () => {
         showOptions ? setShowOptions(false) : setShowOptions(true);
@@ -26,22 +29,23 @@ export const ShowMenuBtn = () => {
                         <div className='small-menu-box'>
                             <Link onClick={() => changeActiveView('home')}
                                   className={(activeView === 'home') ? 'small-menu-btn active' : 'small-menu-btn'}
-                                  to='/'>home</Link>
+                                  to='/'>{desc["main-home-btn"]}</Link>
                             <Link onClick={() => changeActiveView('about-me')}
                                   className={(activeView === 'about-me') ? 'small-menu-btn active' : 'small-menu-btn'}
-                                  to='/about-me'>o mnie</Link>
+                                  to='/about-me'>{desc["main-about-me-btn"]}</Link>
                             <Link onClick={() => changeActiveView('portfolio')}
                                   className={(activeView === 'portfolio') ? 'small-menu-btn active' : 'small-menu-btn'}
-                                  to='/portfolio'>portfolio</Link>
+                                  to='/portfolio'>{desc["main-portfolio-btn"]}</Link>
                             <Link onClick={() => changeActiveView('cooperation')}
                                   className={(activeView === 'cooperation') ? 'small-menu-btn active' : 'small-menu-btn'}
-                                  to='/cooperation'>współpraca</Link>
+                                  to='/cooperation'>{desc["main-cooperation-btn"]}</Link>
                             <Link onClick={() => changeActiveView('technologies')}
                                   className={(activeView === 'technologies') ? 'small-menu-btn active' : 'small-menu-btn'}
-                                  to='/technologies'>technologie</Link>
+                                  to='/technologies'>{desc["main-technologies-btn"]}</Link>
                             <Link onClick={() => changeActiveView('contact')}
                                   className={(activeView === 'contact') ? 'small-menu-btn active' : 'small-menu-btn'}
-                                  to='/contact'>kontakt</Link>
+                                  to='/contact'>{desc["main-contact-btn"]}</Link>
+                            <SelectLanguage/>
                         </div>
                     </>
                 ) : (
