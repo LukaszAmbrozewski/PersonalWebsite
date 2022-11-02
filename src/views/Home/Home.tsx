12318d8601643e0606f3a2e6a0739a7bec83cfa4
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-11-02 09:51:30 +0100

    Changed home view and styles.

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Header} from "../../components/Header/Header";
+import {HomeBox} from "../../components/HomeBox/HomeBox";
+import {Footer} from "../../components/Footer/Footer";
 
 export const Home = () => {
     return(
-        <div>Strona główna</div>
+        <div>
+            <Header/>
+            <HomeBox/>
+            <Footer/>
+        </div>
     )
 }
