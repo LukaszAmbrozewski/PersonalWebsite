f6681860d48fbd17e8f8a953a9215f3cb6d69b1c
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-11-10 09:57:45 +0100

    Added smaller images and configured responsive view.

--- src/components/BackgroundsImageGallery/BackgroundsImageGallery.tsx
+++ src/components/BackgroundsImageGallery/BackgroundsImageGallery.tsx
@@ -5,9 +5,13 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./BackgroundsImageGallery.css";
 
-import { Autoplay, Pagination, Navigation } from "swiper";
+
+import { Autoplay, Navigation } from "swiper";
+import {useMediaQuery} from "react-responsive";
 
 export default function BackgroundsImageGallery() {
+    const smallBackground = useMediaQuery({ maxWidth: 850 })
+
     return (
         <>
             <Swiper
@@ -15,29 +19,26 @@ export default function BackgroundsImageGallery() {
                 loop={true}
                 centeredSlides={true}
 
-                speed={2000}
+                speed={500}
                 autoplay={{
-                    delay: 3000,
+                    delay: 6000,
                     disableOnInteraction: false,
                 }}
-                pagination={{
-                    clickable: false,
-                }}
                 navigation={false}
-                modules={[ Autoplay, Pagination, Navigation]}
+                modules={[ Autoplay, Navigation]}
                 color='red'
             >
                 <SwiperSlide>
-                    <img className='one-photo' src='images/home1.jpg' alt="Slide image 1"/>
+                    <img className='one-photo' src={smallBackground ? 'images/home1-mobile.jpg' : 'images/home1.jpg'} alt="Slide 1"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='one-photo' src='images/home2.jpg' alt="Slide image 2"/>
+                    <img className='one-photo' src={smallBackground ? 'images/home2-mobile.jpg' : 'images/home2.jpg'} alt="Slide 2"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='one-photo' src='images/home3.jpg' alt="Slide image 3"/>
+                    <img className='one-photo' src={smallBackground ? 'images/home3-mobile.jpg' : 'images/home3.jpg'} alt="Slide 3"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='one-photo' src='images/home4.jpg' alt="Slide image 4"/>
+                    <img className='one-photo' src={smallBackground ? 'images/home4-mobile.jpg' : 'images/home4.jpg'} alt="Slide 4"/>
                 </SwiperSlide>
 
             </Swiper>
