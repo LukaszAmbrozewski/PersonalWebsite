40f621c32dc75bf14345f8d389b4f010caefebfe
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2022-11-26 08:38:03 +0100

    Refactored code.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./views/Home/Home";
-import { Contact } from "./views/Contact/Contact";
-import { Portfolio } from "./views/Portfolio/Portfolio";
-import { Cooperation } from "./views/Cooperation/Cooperation";
-import { Technologies } from "./views/Technologies/Technologies";
-import { AboutMe } from "./views/AboutMe/AboutMe";
 import { LanguageContext } from "./contexts/language-context";
 import * as pl from "./lang/pl.json";
 import * as de from "./lang/de.json";
@@ -33,11 +28,6 @@ export const App = () => {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about-me" element={<AboutMe />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/cooperation" element={<Cooperation />} />
-          <Route path="/technologies" element={<Technologies />} />
         </Routes>
       </div>
     </LanguageContext.Provider>
